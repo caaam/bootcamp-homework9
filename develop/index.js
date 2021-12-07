d2e6dea0ed088eff408d3df9f71ef5d3bebc26d8
cam <cam@camherbert.com>
2021-12-06 20:10:28 -0500

    Created writeToFile function

--- develop/index.js
+++ develop/index.js
@@ -1,14 +1,29 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+	"What is the title of the project?",
+    "Describe the application:",
+    "List the installation instructions:",
+    "Give any important usage information:",
+    "Please provide any guidelines for contribution:",
+    "List all instructions for testing the application:"
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+	fs.appendFile(fileName, data, (err) =>
+		err ? console.log(err) : console.log(`file write successful at ${fileName} with data ${data}`)
+	);
+}
 
 // TODO: Create a function to initialize app
 function init() {
 	console.log("is this thing on?");
+	if (fs && inquirer) console.log("packages initialized");
+	writeToFile("readme.txt", "testing");
 }
 
 // Function call to initialize app
