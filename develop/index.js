e24016735f1379cf4b9fde95d7545926fe6bbccf
cam <cam@camherbert.com>
2021-12-13 17:00:26 -0500

    Lots of framework built, just need to output MD data properly

--- develop/index.js
+++ develop/index.js
@@ -1,15 +1,56 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
+// Table of Contents, License, and Questions
 const questions = [
-	"What is the title of the project?",
-    "Describe the application:",
-    "List the installation instructions:",
-    "Give any important usage information:",
-    "Please provide any guidelines for contribution:",
-    "List all instructions for testing the application:"
+	{
+		type: 'input',
+		message: "What is the title of the project?",
+		name: 'title'
+	},
+	{
+		type: 'input',
+		message: "Describe the application:",
+		name: 'description'
+	},
+	{
+		type: 'input',
+		message: "List the installation instructions:",
+		name: 'installInstructions'
+	},
+	{
+		type: 'input',
+		message: "Give any important usage information:",
+		name: 'usageInfo'
+	},
+	{
+		type: 'list',
+		message: "Select a license for the project",
+		name: 'license',
+		choices: [
+			'MIT',
+			'Apache',
+			'none'
+		]
+	},
+	{
+		type: 'input',
+		message: "Please provide any guidelines for contribution:",
+		name: 'contributionGuidelines'
+	},
+	{
+		type: 'input',
+		message: "List all instructions for testing the application:",
+		name: 'testInstructions'
+	},
+	{
+		type: 'input',
+		message: "List any other questions:",
+		name: "questions"
+	}
 ];
 
 // TODO: Create a function to write README file
@@ -21,9 +62,13 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-	console.log("is this thing on?");
 	if (fs && inquirer) console.log("packages initialized");
-	writeToFile("readme.txt", "testing");
+	inquirer.prompt(questions)
+	.then((response) => {
+		console.log(response);
+		writeToFile("readme.md", generateMarkdown(response));
+	});
+	// writeToFile("readme.md", "random");
 }
 
 // Function call to initialize app
--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -1,6 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(!license) { return ""; }
+  if (license === "Apache") { return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"; }
+  if (license === "MIT") { return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"; }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,7 +16,8 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title}\n
+  ${renderLicenseBadge(data.license)}
 
 `;
 }
